66b5d720be6c6427291b055adfe524e350d984f4
Andrey Lashkevich <andlashkevich@ya.ru>
2024-10-29 19:02:32 +0300

    without JSX

--- al-app/src/App.js
+++ al-app/src/App.js
@@ -1,26 +1,37 @@
-import logo from "./logo.svg"; //наверное это императивный стиль
+import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
 function App() {
-	return (
-		//наверное это начинается декларативный стиль и продолжается до закрытия тэга div
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				`{new Date().getFullYear()}`
-			</header>
-		</div>
+	return createElement(
+		"div",
+		{ className: "App" },
+		createElement(
+			"header",
+			{ className: "App-header" },
+			createElement("img", {
+				src: logo,
+				className: "App-logo",
+				alt: "logo",
+			}),
+			createElement(
+				"p",
+				null,
+				"Edit ",
+				createElement("code", null, "src/App.js"),
+				" and save to reload."
+			),
+			createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			new Date().getFullYear()
+		)
 	);
 }
-//возможно это императивный стиль
 export default App;
